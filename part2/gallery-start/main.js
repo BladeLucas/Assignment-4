71b5c3ccb367cb73580dc2643e6709f58616e60e
Blade Lucas <elffe2@hotmail.ca>
2024-12-03 16:15:41 -0500

    followed tutorial for part 2 and made all functions properly work

--- part2/gallery-start/main.js
+++ part2/gallery-start/main.js
@@ -5,14 +5,33 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const picNames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
+const picAlts = ["Pic1", "Pic2", "Pic3", "Pic4", "Pic5"];
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (const x of picNames){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${x}`);
+    newImage.setAttribute('alt', picAlts[x]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener("click", e => {
+        displayedImage.src = e.target.src; 
+        displayedImage.alt = e.target.alt;
+    })
+}
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  });
